8ae923a9f1dceae051b4c2f01269f86691b1d1d2
GlobyteFranco <franco.cipulli@globallogic.com>
2026-05-09 22:31:01 -0300

    Minor Changes

--- model/Curso.java
+++ model/Curso.java
@@ -3,3 +3,7 @@ package model;
 public class Curso {
 
 }
+// Leerse lo que pregunte sobre la posible clase vista a gemini.
+// Seguir con el programa por ese lado.
+// Analizar como aplicar correctamente, o si ya lo estoy haciendo, el modelo MVC
+// en este proyecto asi como investigar mas sobre este.
